596980d1076fa1f32c9c2075854099213994ed42
Yury <goldyukol12@gmail.com>
2020-02-06 17:15:07 +0200

    add color

--- src/Components/Color/Color.tsx
+++ src/Components/Color/Color.tsx
@@ -1,16 +1,25 @@
-
-
-import React from 'react';
-
+import React, { CSSProperties, useState } from 'react';
 interface IProps {
 
 }
 
 const Color = (props: IProps) => {
+
+    const [color, setColor] = useState('#00ffff')
+
     return (
-        <div>
-            Forgot something
-    </div>
+        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+            <div>Color</div>
+            <div><input type="color" value={color} onChange={(e) => setColor(e.currentTarget.value)} /></div>
+            <div style={{ width: '100px', height: '100px', background: color }}></div>
+            <div
+                style={{
+                    width: '300px',
+                    height: '100px',
+                    background: `linear-gradient(to top, #E4AF9D 20%, #E4E4D8 50%, #A19887 80%)`,
+                }}
+            />
+        </div >
     )
 
 };
